aaa3af2136fd5cedaca55c8ddc1513c6dabf6de8
XinJieSoh <sohxinjie518@gmail.com>
2024-06-17 16:38:17 +0800

    Add swagger description

--- src/main/java/com/soh/EmployeeManagementSystem/EmployeeManagementSystemApplication.java
+++ src/main/java/com/soh/EmployeeManagementSystem/EmployeeManagementSystemApplication.java
@@ -1,9 +1,23 @@
 package com.soh.EmployeeManagementSystem;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Employee Management System",
+				version = "1.0.0",
+				description ="The objective of this project is to create a simple yet comprehensive Employee Management System using Java with Spring Boot. The system will manage employees across different departments within a company. It will utilize a relational database to store employee details, department information, and project assignments. This system will expose a RESTful API for performing CRUD operations on the employee data.",
+				contact = @Contact(
+						name ="Soh Xin Jie",
+						email = "sohxinjie518@gmail.com"
+				)
+		)
+)
 public class EmployeeManagementSystemApplication {
 
 	public static void main(String[] args) {
